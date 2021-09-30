849d95b896f2ac2e9e9eef60666e751c2d3f48ea
Jon Foster <jon@jfpossibilities.com>
2021-09-30 06:47:52 -0700

    Type corrections from KenUNIX

--- compile.bat
+++ compile.bat
@@ -1,7 +1,12 @@
 @echo off
-echo 8-5-2020 Ken. bwbasic. Works under Windows 10 using gcc 7.4.0
-echo                         and gcc 8.1.0 64 bit
-echo                                Wait
+echo 9-20-2021 Ken. bwbasic. Works under Windows 10 using gcc 7.4.0
+echo                              and gcc 8.1.0 64 bit
+echo            and Windows 11 using tdm64-gcc-5.1.0-2 gcc 5.1.0 32 and 64 bit
+echo https://sourceforge.net/projects/tdm-gcc/files/TDM-GCC%20Installer/tdm64-gcc-5.1.0-2.exe/download
+echo                           gcc 5.1.0 works with XP, 7 and 10
+echo                             added notes for 32 bit
+echo.
+echo                                      Wait
 echo.
 
 if exist *.o del *.o
@@ -25,8 +30,10 @@ call stdcomp bwx_tty.c
 call stdcomp bwd_cmd.c
 call stdcomp bwd_fun.c
 
+Rem If compiling under DOS 32 bit (gcc 5.1.0) add -m32 after -s
 gcc -s -ansi -DMSDOS -o bwbasic.exe bwb_cmd.o bwb_cnd.o bwb_dio.o bwb_exp.o bwb_fnc.o bwb_inp.o bwb_int.o bwb_prn.o bwb_stc.o bwb_str.o bwb_tbl.o bwb_var.o bwbasic.o bwd_cmd.o bwd_fun.o bwx_tty.o
 
+Rem If compiling under DOS 32 bit (gcc 5.1.0) add -m32 after -s
 gcc -s -ansi -DMSDOS -o renum.exe renum.c 
 
 if exist "renum.exe" (
--- stdcomp.bat
+++ stdcomp.bat
@@ -1,1 +1,5 @@
+Rem If compiling under DOS 32 bit (gcc 5.1.0) add -m32 after -w
+Rem If compiling under DOS 32 bit (gcc 5.1.0) add -m32 after -w
 gcc -w -c -ansi -DMSDOS -I . %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+
