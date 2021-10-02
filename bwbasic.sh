11071464c60e077e045ddc683786a174f724896b
kenunix <ken.unix.man@protonmail.com>
2021-10-02 15:21:41 -0400

    Additions to Makefile addgui and delgui and typos Updated bwbasic.sh typo and bwbasic.desktop Change bwbasic.png

--- bwbasic.sh
+++ bwbasic.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # 12/14/2019 Ken
 # 07/28/2020 enhanced by ChipMaster
+# 10/02/2021 fix typo kenunix
 [ -z "$BWBASIC" ]   && BWBASIC="$HOME/bwbasic"
 [ ! -d "$BWBASIC" ] && mkdir "$BWBASIC"
 cd "$BWBASIC"
-trap '/bin/echo " "; /bin/echo -e "PROGRAM INTERRUPTED. \007Press Enter "; read j; exit 1' INT
+trap '/bin/echo " "; /bin/echo -e -n "PROGRAM INTERRUPTED. \007Press Enter "; read j; exit 1' INT
 bwbasic "$@"
 # -n = no newline -e interpret special octel numbers \007 = Bell
 /bin/echo -n "Bwbasic terminated. Press Enter "
