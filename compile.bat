95a58ee5bcb75371cd21636d39bc12281b6dc90a
kenunix <ken.unix.man@protonmail.com>
2021-10-02 08:53:32 -0400

    compile.bat updated for 32/64 bit XP,7,10,11

--- compile.bat
+++ compile.bat
@@ -1,7 +1,14 @@
 @echo off
-echo 8-5-2020 Ken. bwbasic. Works under Windows 10 using gcc 7.4.0
-echo                         and gcc 8.1.0 64 bit
-echo                                Wait
+echo 10-02-2021 Ken. bwbasic  compile.bat
+echo.
+echo                     Tested under Windows XP , 7 ,10 and 11
+echo                             added notes for 32 bit
+echo.
+echo Source - https://sourceforge.net/projects/tdm-gcc/files/TDM-GCC%20Installer/tdm64-gcc-5.1.0-2.exe/download
+echo.
+echo                           Tested under bwbasic-3.20d
+echo.
+echo                                      Wait
 echo.
 
 if exist *.o del *.o
@@ -25,8 +32,10 @@ call stdcomp bwx_tty.c
 call stdcomp bwd_cmd.c
 call stdcomp bwd_fun.c
 
+Rem If compiling under DOS 32 bit (gcc 5.1.0) add -m32 after -s
 gcc -s -ansi -DMSDOS -o bwbasic.exe bwb_cmd.o bwb_cnd.o bwb_dio.o bwb_exp.o bwb_fnc.o bwb_inp.o bwb_int.o bwb_prn.o bwb_stc.o bwb_str.o bwb_tbl.o bwb_var.o bwbasic.o bwd_cmd.o bwd_fun.o bwx_tty.o
 
+Rem If compiling under DOS 32 bit (gcc 5.1.0) add -m32 after -s
 gcc -s -ansi -DMSDOS -o renum.exe renum.c 
 
 if exist "renum.exe" (
